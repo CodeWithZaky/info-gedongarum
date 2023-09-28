b9953c122066f63c977fc5e5eff26d2fc9a4a20d
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2023-09-28 20:35:27 +0700

    navlink data

---
+++ src/components/datas/Navlinks.tsx
@@ -0,0 +1,14 @@
+export const navLinks: { name: string; link: string }[] = [
+  {
+    name: "Beranda",
+    link: "/",
+  },
+  {
+    name: "Profil",
+    link: "/profil",
+  },
+  {
+    name: "galeri",
+    link: "/galeri",
+  },
+];
