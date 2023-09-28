a9f29703979005ced925bcc8f3cbdfbfc42a1f0b
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2023-09-28 20:35:38 +0700

    village datas

---
+++ src/components/datas/VilData.ts
@@ -0,0 +1,36 @@
+export const VilData: { title: string; list: string[] }[] = [
+  {
+    title: "Lembaga",
+    list: [
+      "Badan Permusyawaratan Desa",
+      "PKK",
+      "Karang Taruna",
+      "LPMD",
+      "Linmas",
+      "RT-RW",
+      "Kelompok Tani",
+      "Kelompok Informasi Masyarakat",
+      "Badan Usaha Milik Desa",
+      "Perpustakaan Desa",
+      "Badan Amil Zakat",
+    ],
+  },
+  {
+    title: "Layanan",
+    list: [
+      "Program Petani Mandiri",
+      "Kartu Pedagang Produktif",
+      "Beasiswa",
+      "Penerbitan KTP",
+      "Penerbitan KK",
+      "Pembuatan Akta",
+      "PKH",
+      "BPJS Kesehatan",
+      "Santunan Kematian",
+    ],
+  },
+  {
+    title: "Potensi",
+    list: ["Bidang Pertanian", "Bidang Pangan", "Bidang Peternakan/Perikanan"],
+  },
+];
