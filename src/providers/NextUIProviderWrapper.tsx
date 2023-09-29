41f775d476fba2a8c77d630e57efeafcc0229abe
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2023-09-29 19:41:23 +0700

    remove as change directory

--- src/providers/NextUIProviderWrapper.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { NextUIProvider } from "@nextui-org/react";
-
-const NextUIProviderWrapper = ({ children }: { children: React.ReactNode }) => {
-  return <NextUIProvider>{children}</NextUIProvider>;
-};
-
-export default NextUIProviderWrapper;
