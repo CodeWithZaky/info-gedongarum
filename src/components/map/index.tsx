70868a69b3581a5ae5c1db36579151c02bd36ab4
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2023-09-29 19:42:13 +0700

    add map section

---
+++ src/components/map/index.tsx
@@ -0,0 +1,15 @@
+const index = () => {
+  return (
+    <div className="relative w-full h-96 my-10">
+      <iframe
+        className="rounded-lg"
+        width="100%"
+        height="100%"
+        title="map"
+        src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"
+      ></iframe>
+    </div>
+  );
+};
+
+export default index;
