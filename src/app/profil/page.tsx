30f279d9b3a1c75030e48a8fce423fcf653fff5d
Ahmad Zaky Ubaidillah <111162218+zakyscripters@users.noreply.github.com>
2023-10-14 21:04:05 +0700

    add profile content

--- src/app/profil/page.tsx
+++ src/app/profil/page.tsx
@@ -1,7 +1,31 @@
-import React from "react";
-
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="flex flex-col items-start gap-3 w-full px-3 text-xs sm:w-[90%] sm:text-sm md:w-[80%] md:text-base lg:text-lg xl:text-xl">
+      <p className="leading-5 sm:leading-6 md:leading-8">
+        Desa “Gedongarum” pada tempo dulu merupakan daerah hutan belantara yang
+        terletak di bantaran sungai Bengawan Solo yang banyak ditumbuhi
+        pepohonan, diantaranya pohon “Gebang” dan pohon “Kedondong”.Datanglah
+        seorang pengembara yang bernama “Mbah Wukir” & “Mbah Lajer” berjuang
+        untuk membuka lahan menebang pepohonan (Babat alas) untuk tempat tinggal
+        dan bercocok tanam.Oleh karena disebelah barat dusun banyak ditumbuhi
+        pohon Gebang, maka Mbah Wukir memberikan nama dusun “Gebang” dan bagian
+        Timur yang banyak pohon kedondong Mbah Lajer memberi nama Dusun
+        “Dondong”.
+      </p>
+      <p className="leading-5 sm:leading-6 md:leading-8">
+        Nama “Gedongarum” berasal dari penggabungan dari 2 nama Dusun yaitu
+        “Gebang” & Dondong, sehingga menjadi “GEDONG” dan “ARUM” yang memberikan
+        makna semerbak harum, kelak Desa Gedongarum akan menjadi sebuah Desa
+        yang terkenal oleh karena kemajuan Pembangunan Desa, sehingga menjadi
+        Desa yang Gemah ripah Loh Jinawi Toto Tentrem Karta Raharjo.Hingga
+        sekarang “Mbah Wukir” & “Mbah Lajer” diyakini warga masyarakat Desa
+        Gedongarum sebagai “Danyang” (orang pertama kali yang bertempat tinggal
+        / Babat alas di Desa Gedongarum) dan makam petilasannya hingga saat ini
+        masih dilestarikan, sehingga makam tersebut dibuatkan “Cungkup” (rumah
+        kecil).
+      </p>
+    </div>
+  );
 };
 
 export default page;
