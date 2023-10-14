e4ca2b34bad1835f6f69544200488943eb35155f
Ahmad Zaky Ubaidillah <111162218+zakyscripters@users.noreply.github.com>
2023-10-14 21:15:00 +0700

    change map to Gedongarum village

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -6,7 +6,7 @@ const index = () => {
         width="100%"
         height="100%"
         title="map"
-        src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3948.3627271982486!2d111.90323831429168!3d-7.141548478474566!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78ab6f80868671%3A0xb3d49888a93e58d9!2sGedongarum%2C%20Kec.%20Kanor%2C%20Bojonegoro%2C%20Jawa%20Timur!5e0!3m2!1sen!2sid!4v1665913573577!5m2!1sen!2sid"
       ></iframe>
     </div>
   );
