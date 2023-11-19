9b4f048a7474aa97985e4a32ccd3f7ca7c4f790a
Ahmad Zaky Ubaidillah <111162218+zakyscripters@users.noreply.github.com>
2023-11-19 19:32:35 +0700

    update

--- src/components/galery/GaleryGrid.tsx
+++ src/components/galery/GaleryGrid.tsx
@@ -1,23 +1,10 @@
 import Image from "next/image";
+import { imageData } from "./ImageData";
 
-const GaleryGrid = ({ itemsPerPage }: { itemsPerPage: number }) => {
+const GaleryGrid = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="columns-2 sm:columns-2 md:columns-3 lg:columns-4 p-5">
-      {[...Array(itemsPerPage)].map((_, i) => (
-        <div
-          key={i}
-          className="relative mb-4 before:content-[''] before:rounded-md before:absolute before:inset-0 before:bg-black before:bg-opacity-20 bg-card"
-        >
-          <Image
-            alt="image"
-            height={500}
-            width={500}
-            objectFit={"cover"}
-            className="w-full rounded-md"
-            src={`https://source.unsplash.com/${600 + i + 1}x${600 + i}`}
-          />
-        </div>
-      ))}
+    <div className="columns-2 sm:columns-2 md:columns-3 lg:columns-3 p-5">
+      {children}
     </div>
   );
 };
