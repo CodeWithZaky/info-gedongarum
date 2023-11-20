dcf6f309d3b0175194e6d64fc51768b0669c2308
Ahmad Zaky Ubaidillah <111162218+zakyscripters@users.noreply.github.com>
2023-11-20 10:36:51 +0700

    remove unused

--- src/components/galery/GaleryGrid.tsx
+++ src/components/galery/GaleryGrid.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import { imageData } from "./ImageData";
-
 const GaleryGrid = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="columns-2 sm:columns-2 md:columns-3 lg:columns-3 p-5">
