7f97de28d6f1c62b1407b97effd0139a41cca748
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2025-02-07 18:07:43 +0700

    remove "oleh" route and update ui toggle button theme

--- src/components/nav/Navlinks.tsx
+++ src/components/nav/Navlinks.tsx
@@ -1,18 +1,14 @@
 export const navLinks: { name: string; link: string }[] = [
-    {
-        name: "Beranda",
-        link: "/",
-    },
-    {
-        name: "Profil",
-        link: "/profil",
-    },
-    {
-        name: "galeri",
-        link: "/galeri",
-    },
-    {
-        name: "Oleh",
-        link: "/oleh",
-    },
+  {
+    name: "Beranda",
+    link: "/",
+  },
+  {
+    name: "Profil",
+    link: "/profil",
+  },
+  {
+    name: "galeri",
+    link: "/galeri",
+  },
 ];
